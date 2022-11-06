3e46d9d76516fb713a5fce50c65e09f6c47504ce
Rajpurohit Dhanpalm <rajpurohitdhanpal@gmail.com>
2022-11-06 22:09:10 +0530

    login api

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,4 @@
-"""hiringsystem URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/', include('userAccounts.urls')),
+    path('api/v1/accounts/', include('userAccounts.urls')),
 
 ]
