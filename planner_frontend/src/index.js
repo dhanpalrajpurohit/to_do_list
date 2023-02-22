88b64c9061c2b9fd0f53fb920604da3f30152346
Dhanpal Rajpurohit <rajpurohitdhanpal@gmail.com>
2023-02-23 00:05:13 +0530

    adding redux for auth api

--- planner_frontend/src/index.js
+++ planner_frontend/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from 'react-redux'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {store} from './store/reducer';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+  </Provider>
   </React.StrictMode>
 );
 
